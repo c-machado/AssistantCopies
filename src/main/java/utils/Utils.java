15e4064f3cb9b9848a69d0da8599d3b6b2b67ad0
Agustin Villar <agustin.v@endouble.com>
2019-06-17 21:19:53 +0200

    add util to clean up HTML strings

---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,10 @@
+package utils;
+
+import org.jsoup.Jsoup;
+
+public class Utils {
+    /** This util will remove any HTML tag from a given HTML string  */
+    public static String htmlToText(String html) {
+        return Jsoup.parse(html).text();
+    }
+}
