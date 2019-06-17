6428568f8129289d2c7bd6f46ce6a38f33e2c4f2
Agustin Villar <agustin.v@endouble.com>
2019-06-17 21:20:50 +0200

    add HTML stripping sample

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,6 +1,8 @@
 import base.Homepage;
 import org.testng.annotations.Test;
 import base.SheetsQuickstart;
+import utils.Utils;
+
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
@@ -16,6 +18,20 @@ public class TestRunner {
 //        home = new Homepage();
 //        home.setUp();
         System.out.println("test execution");
+
+
+        /** TODO: remove this code block */
+        /** This is just a sample to check out how the html parser works */
+        String myHTMLString = "<a href='https://assistant.google.com/learn/' class='h-c-link' data-g-event='devices-section' ";
+               myHTMLString += "data-g-action='clicked' data-g-label='cta:learn-page'>Learn more</a> about how you can get ";
+               myHTMLString += "help from your Google&nbsp;Assistant.";
+
+
+        /** Here you will see the html string with any tags it might have */
+        System.out.println("HTML with tags: " + myHTMLString);
+        /** Here you will see the cleaned string without any tags */
+        System.out.println("HTML string without tags: " + Utils.htmlToText(myHTMLString));
+
         try {
             sheets = new SheetsQuickstart();
             sheets.authenticate();
