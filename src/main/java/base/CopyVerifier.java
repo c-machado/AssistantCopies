53731051d18ad4f3f3c1874f833125ca5ca89b49
Agustin Villar <agustin.v@endouble.com>
2019-06-21 11:05:39 +0200

    reformat code

--- src/main/java/base/CopyVerifier.java
+++ src/main/java/base/CopyVerifier.java
@@ -12,33 +12,33 @@ import java.util.concurrent.TimeUnit;
 public class CopyVerifier {
     private static WebDriver driver;
 
-    public CopyVerifier(){
+    public CopyVerifier() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         driver.get("https://assistant.google.com/");
         //home = new Home_Page(driver);
     }
 
-    public String getCopy(String _selector){
+    public String getCopy(String _selector) {
         String dataType = "";
-        String [] selectorKeys = _selector.split(",");
+        String[] selectorKeys = _selector.split(",");
         String selector = selectorKeys[0];
-        if(selectorKeys.length > 1) {
+        if (selectorKeys.length > 1) {
             dataType = selectorKeys[1];
         }
         WebElement element = driver.findElement(By.cssSelector(selector));
         String tag = element.getTagName();
-        System.out.println("tag " +tag);
+        System.out.println("tag " + tag);
 
-        switch (tag){
+        switch (tag) {
             case "img":
-                System.out.println("Case IMG " + getCopyFromType(dataType,element));
-                return getCopyFromType(dataType,element);
+                System.out.println("Case IMG " + getCopyFromType(dataType, element));
+                return getCopyFromType(dataType, element);
             case "a":
-                System.out.println("Case a " + getCopyFromType(dataType,element));
-                return getCopyFromType(dataType,element);
+                System.out.println("Case a " + getCopyFromType(dataType, element));
+                return getCopyFromType(dataType, element);
             case "div":
-                System.out.println("Case div "+element.getAttribute("data-c-video-id"));
+                System.out.println("Case div " + element.getAttribute("data-c-video-id"));
                 String youTubeVideo = "https://youtube.com/watch?v=" + element.getAttribute("data-c-video-id");
                 return youTubeVideo;
             case "span":
@@ -52,8 +52,8 @@ public class CopyVerifier {
         }
     }
 
-    public String getCopyFromType(String _dataType, WebElement _element){
-        switch (_dataType){
+    public String getCopyFromType(String _dataType, WebElement _element) {
+        switch (_dataType) {
             case "text":
                 System.out.println("Case TEXT " + _element.getAttribute("innerText"));
                 return _element.getAttribute("innerText").trim();
@@ -65,13 +65,13 @@ public class CopyVerifier {
                 return _element.getAttribute("data-src");
             case "alt":
                 System.out.println("Case altText " + _element.getAttribute("alt"));
-                return  _element.getAttribute("alt");
+                return _element.getAttribute("alt");
             default:
-                throw new RuntimeException("unknown locator 1 " +_element);
+                throw new RuntimeException("unknown locator 1 " + _element);
         }
     }
 
-    public void compareCopies(String _selector, String _copyOnSheets){
+    public void compareCopies(String _selector, String _copyOnSheets) {
         Assert.assertEquals(getCopy(_selector), _copyOnSheets, "Copies match");
     }
 }
